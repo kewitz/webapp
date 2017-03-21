3bfcd44dac46252c7a9b5c6f9a6dbc589fa19e5f
Ryan Boyajian <ryan.e.boyajian@gmail.com>
2017-03-21 09:13:25 -0600

    Redirect onboarding to the home if already seen
    [Finishes #141746125](https://www.pivotaltracker.com/story/show/141746125)

--- src/routes/onboarding/index.js
+++ src/routes/onboarding/index.js
@@ -2,31 +2,45 @@ import OnboardingCategoriesContainer from '../../containers/OnboardingCategories
 import OnboardingCollaborateContainer from '../../containers/OnboardingCollaborateContainer'
 import OnboardingInvitationsContainer from '../../containers/OnboardingInvitationsContainer'
 import OnboardingSettingsContainer from '../../containers/OnboardingSettingsContainer'
+import { ONBOARDING_VERSION } from '../../constants/application_types'
 
-export default [
-  {
-    path: 'onboarding/categories',
-    getComponent(location, cb) {
-      cb(null, OnboardingCategoriesContainer)
+export default (store) => {
+  function onEnter(nextState, replace) {
+    const state = store.getState()
+    if (state.profile.get('webOnboardingVersion') === ONBOARDING_VERSION) {
+      replace({ pathname: state.gui.get('homeStream'), state: nextState })
+    }
+  }
+
+  return [
+    {
+      path: 'onboarding/categories',
+      getComponent(location, cb) {
+        cb(null, OnboardingCategoriesContainer)
+      },
+      onEnter,
     },
-  },
-  {
-    path: 'onboarding/settings',
-    getComponent(location, cb) {
-      cb(null, OnboardingSettingsContainer)
+    {
+      path: 'onboarding/settings',
+      getComponent(location, cb) {
+        cb(null, OnboardingSettingsContainer)
+      },
+      onEnter,
     },
-  },
-  {
-    path: 'onboarding/collaborate',
-    getComponent(location, cb) {
-      cb(null, OnboardingCollaborateContainer)
+    {
+      path: 'onboarding/collaborate',
+      getComponent(location, cb) {
+        cb(null, OnboardingCollaborateContainer)
+      },
+      onEnter,
     },
-  },
-  {
-    path: 'onboarding/invitations',
-    getComponent(location, cb) {
-      cb(null, OnboardingInvitationsContainer)
+    {
+      path: 'onboarding/invitations',
+      getComponent(location, cb) {
+        cb(null, OnboardingInvitationsContainer)
+      },
+      onEnter,
     },
-  },
-]
+  ]
+}
 
