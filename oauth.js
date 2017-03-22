1ce51f30d549e0d2c19794a76c41a5462d58e878
Jay Zeschin <jay.zeschin@modeset.com>
2017-03-22 13:19:45 -0600

    OAuth2 shim should pull config from global ENV
    It was previously memoized locally and couldn’t be changed on the fly. This makes it pull directly from `global.ENV`, consistent with the rest of the application.
    
    Ensure that authentication actions load AUTH_CLIENT_ID dynamically too.

--- oauth.js
+++ oauth.js
@@ -1,46 +1,42 @@
-// load env vars first
-require('dotenv').load({ silent: process.env.NODE_ENV === 'production' })
-
-// Auth token
-// Get the access token object.
-const credentials = {
-  client: {
-    id: process.env.AUTH_CLIENT_ID,
-    secret: process.env.AUTH_CLIENT_SECRET,
-  },
-  auth: {
-    tokenHost: process.env.AUTH_DOMAIN,
-    tokenPath: '/api/oauth/token',
-  },
-  http: {
-    headers: {
-      Accept: 'application/json',
-    },
-  },
-}
+const OAuth2 = require('simple-oauth2')
 
 // Initialize the OAuth2 Library
-const oauth2 = require('simple-oauth2').create(credentials)
-
 const tokenConfig = { scope: 'public scoped_refresh_token' }
 let token = null
 
 export function currentToken() {
   if (!token || token.expired()) {
+    const credentials = {
+      client: {
+        id: ENV.AUTH_CLIENT_ID,
+        secret: ENV.AUTH_CLIENT_SECRET,
+      },
+      auth: {
+        tokenHost: ENV.AUTH_DOMAIN,
+        tokenPath: '/api/oauth/token',
+      },
+      http: {
+        headers: {
+          Accept: 'application/json',
+        },
+      },
+    }
+
+    const client = OAuth2.create(credentials)
     return new Promise((resolve) => {
-      oauth2.clientCredentials
-      .getToken(tokenConfig)
-      .then((result) => {
-        if (result.errors) {
-          console.log('Unable to get access token', result)
+      client.clientCredentials
+        .getToken(tokenConfig)
+        .then((result) => {
+          if (result.errors) {
+            console.log('Unable to get access token', result)
+            process.exit(1)
+          }
+          token = client.accessToken.create(result)
+          resolve(token)
+        }).catch((err) => {
+          console.log('Unable to get access token', err)
           process.exit(1)
-        }
-        token = oauth2.accessToken.create(result)
-        resolve(token)
-      }).catch((err) => {
-        console.log('Unable to get access token', err)
-        process.exit(1)
-      })
+        })
     })
   }
   return new Promise((resolve) => { resolve(token) })
