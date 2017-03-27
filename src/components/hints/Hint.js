bcf06c6d3132c537bbf6193a6e83f911e8bbb6f4
Matthew Kitt <mk@mkitt.net>
2017-03-27 17:17:11 -0600

    Migrate Hints to jss
    [#138191285](https://www.pivotaltracker.com/story/show/138191285)

--- src/components/hints/Hint.js
+++ src/components/hints/Hint.js
@@ -1,8 +1,57 @@
 import React, { PropTypes } from 'react'
 import classNames from 'classnames'
+import { css, parent } from '../../styles/jss'
+import {
+  absolute,
+  bgcBlack,
+  colorWhite,
+  displayNone,
+  hidden,
+  inlineBlock,
+  nowrap,
+  opacity0,
+  opacity1,
+  pointerNone,
+  visible,
+} from '../../styles/jso'
+
+const showHintStyle = css(
+  visible,
+  opacity1,
+  {
+    transitionDelay: '0.5s, 0s',
+    transitionDuration: '0.4s, 0s',
+  },
+)
+
+const hintStyle = css(
+  displayNone,
+  hidden,
+  absolute,
+  {
+    top: -25,
+    left: 0,
+    height: 22,
+    padding: '0 11px',
+    lineHeight: '22px',
+    borderRadius: 11,
+  },
+  colorWhite,
+  nowrap,
+  pointerNone,
+  bgcBlack,
+  opacity0,
+  {
+    border: '1px solid #000',
+    transition: 'opacity 0.2s ease, visibility 0s ease 0.2s, color 0.2s ease',
+  },
+  parent('.no-touch', inlineBlock),
+  parent('.no-touch a:hover >', showHintStyle),
+  parent('.no-touch button:hover >', showHintStyle),
+)
 
 const Hint = ({ className, children }) =>
-  <span className={classNames('Hint', className)}>{children}</span>
+  <span className={classNames('Hint', `${hintStyle}`, className)}>{children}</span>
 
 Hint.propTypes = {
   children: PropTypes.string.isRequired,
