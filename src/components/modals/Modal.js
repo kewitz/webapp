3866c6bf79c656b3c91a7617c4b464bf0d25a284
Matthew Kitt <mk@mkitt.net>
2017-03-27 10:32:41 -0600

    Add a close button to all modals
    This adds the button to all existing modals so users have a visual cue
    on something to click on in order to dismiss a modal.
    
    [Fixes: #141971643](https://www.pivotaltracker.com/story/show/141971643)

--- src/components/modals/Modal.js
+++ src/components/modals/Modal.js
@@ -1,11 +1,12 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
-
 import React, { PropTypes } from 'react'
 import classNames from 'classnames'
+import { XIcon } from '../assets/Icons'
 
 export const Modal = ({ classList, component, kind, isActive, onClickModal }) =>
   <div className={classNames(classList, kind, { isActive })} onClick={onClickModal}>
     {component}
+    <button className="CloseModal XClose"><XIcon /></button>
   </div>
 
 Modal.propTypes = {
