1d0600e05ccf206eb7d398536cd9ea3bc031c331
Matthew Kitt <mk@mkitt.net>
2017-03-30 12:10:39 -0600

    Migrate ConfirmDialog to jss
    [#138191285](https://www.pivotaltracker.com/story/show/138191285)

--- src/components/dialogs/ConfirmDialog.js
+++ src/components/dialogs/ConfirmDialog.js
@@ -1,10 +1,53 @@
 import React, { PropTypes } from 'react'
+import { css, hover, media, modifier, select } from '../../styles/jss'
+import {
+  alignMiddle,
+  bgcBlack,
+  bgcWhite,
+  borderWhite,
+  center,
+  colorBlack,
+  colorWhite,
+  ease,
+  fontSize14,
+  fontSize18,
+  fontSize24,
+  inlineBlock,
+  mb0,
+  mb10,
+  minBreak2,
+  mr20,
+  nowrap,
+} from '../../styles/jso'
+
+const dialogStyle = css(center)
+// TODO: exports used by DeleteAccountDialog, move to Dialog
+export const headingStyle = css(
+  { height: 30, lineHeight: '30px' }, mb10, fontSize18, nowrap,
+  media(minBreak2, inlineBlock, mr20, mb0, fontSize24, alignMiddle),
+)
+const buttonHighlightStyle = css(colorWhite, bgcBlack, { borderColor: '#000' })
+export const buttonStyle = css(
+  inlineBlock,
+  { width: 60, height: 40, lineHeight: '40px', borderRadius: 20 },
+  fontSize14,
+  colorBlack,
+  center,
+  nowrap,
+  alignMiddle,
+  bgcWhite,
+  borderWhite,
+  { transition: `background-color 0.2s ${ease}, border-color 0.2s ${ease}, color 0.2s ${ease}, opacity 0.2s ${ease}` },
+  modifier('.isActive', buttonHighlightStyle),
+  hover(buttonHighlightStyle),
+  select('& + &', { marginLeft: 5 }),
+)
 
 const ConfirmDialog = ({ title, onConfirm, onDismiss }) =>
-  <div className="Dialog ConfirmDialog">
-    {title ? <h2>{title}</h2> : null}
-    <button className="ConfirmDialogButton" onClick={onConfirm}>Yes</button>
-    <button className="ConfirmDialogButton" onClick={onDismiss}>No</button>
+  <div className={`Dialog ${dialogStyle}`}>
+    {title ? <h2 className={headingStyle}>{title}</h2> : null}
+    <button className={buttonStyle} onClick={onConfirm}>Yes</button>
+    <button className={buttonStyle} onClick={onDismiss}>No</button>
   </div>
 
 ConfirmDialog.propTypes = {
