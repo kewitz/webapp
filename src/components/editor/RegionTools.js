0479a3a1eab700deda2933fab29d1a4c694307f6
Matthew Kitt <mk@mkitt.net>
2017-04-03 04:41:29 -0600

    Migrate RegionTools to jss
    [#138191285](https://www.pivotaltracker.com/story/show/138191285)

--- src/components/editor/RegionTools.js
+++ src/components/editor/RegionTools.js
@@ -1,12 +1,43 @@
-import React, { Component, PropTypes } from 'react'
+import React, { PropTypes, PureComponent } from 'react'
 import { connect } from 'react-redux'
 import { openModal, closeModal } from '../../actions/modals'
 import { DragIcon, XIcon } from '../assets/Icons'
 import ConfirmDialog from '../dialogs/ConfirmDialog'
+import { css, hover, parent } from '../../styles/jss'
+import {
+  absolute,
+  colorA,
+  colorBlack,
+  displayNone,
+  inlineBlock,
+  rotate90,
+  zIndex2,
+} from '../../styles/jso'
 
-/* eslint-disable react/prefer-stateless-function */
-class RegionTools extends Component {
+const toolsStyle = css(
+  absolute,
+  { top: 5, right: 5 },
+  zIndex2,
+  colorA,
+  parent('.BlockPlaceholder >', displayNone),
+  parent('.ZeroState', displayNone),
+)
 
+const dragButtonStyle = css(
+  displayNone,
+  rotate90,
+  { cursor: 'move' },
+  hover(colorBlack),
+  parent('.no-touch', inlineBlock),
+  parent('.editor-region[data-num-blocks="1"]', displayNone),
+)
+
+const removeButtonStyle = css(
+  hover(colorBlack),
+  parent('.editor:not(.hasContent)', displayNone),
+)
+
+class RegionTools extends PureComponent {
   static propTypes = {
     dispatch: PropTypes.func.isRequired,
     editorId: PropTypes.string.isRequired,
@@ -37,17 +68,11 @@ class RegionTools extends Component {
   render() {
     const { editorId } = this.props
     return (
-      <div className="RegionTools">
-        <button
-          className="BlockRemove"
-          onClick={this.handleDeleteBlock}
-        >
+      <div className={`RegionTools ${toolsStyle}`}>
+        <button className={`BlockRemove ${removeButtonStyle}`} onClick={this.handleDeleteBlock}>
           <XIcon />
         </button>
-        <button
-          className="DragHandler"
-          data-drag-id={editorId}
-        >
+        <button className={`DragHandler ${dragButtonStyle}`} data-drag-id={editorId}>
           <DragIcon />
         </button>
       </div>
--- src/components/editor/RepostBlock.js
+++ src/components/editor/RepostBlock.js
@@ -3,6 +3,8 @@
 import React, { Component, PropTypes } from 'react'
 import { connect } from 'react-redux'
 import { LockIcon, RepostIcon } from '../assets/Icons'
+import { css } from '../../styles/jss'
+import { absolute, colorA, zIndex2 } from '../../styles/jso'
 
 function mapStateToProps({ profile: currentUser }) {
   return { currentUser }
@@ -28,6 +30,8 @@ function getBlockElement(block, uid) {
   }
 }
 
+const toolsStyle = css(absolute, { top: 5, right: 5 }, zIndex2, colorA)
+
 class RepostBlock extends Component {
 
   static propTypes = {
@@ -48,7 +52,7 @@ class RepostBlock extends Component {
           {` by @${currentUser.get('username')}`}
         </div>
         {data.map((block, i) => getBlockElement(block, i))}
-        <div className="RegionTools">
+        <div className={toolsStyle}>
           <LockIcon />
         </div>
       </div>
