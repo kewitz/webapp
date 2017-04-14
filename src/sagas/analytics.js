dbaa260870d13cc9bf12b146a17418907d64b535
Ryan Boyajian <ryan.e.boyajian@gmail.com>
2017-04-14 12:06:35 -0600

    Actually fire off multiple track events, oops

--- src/sagas/analytics.js
+++ src/sagas/analytics.js
@@ -33,69 +33,90 @@ function* trackEvent() {
 function* trackEvents() {
   while (true) {
     const action = yield take('*')
-    const method = get(action, 'payload.method')
-    const isFeatured = yield select(selectProfileIsFeatured)
     switch (action.type) {
       case ACTION_TYPES.ALERT.OPEN:
       case ACTION_TYPES.MODAL.OPEN:
         if (get(action, 'payload.trackLabel')) {
-          return yield put(trackEventAction(get(action, 'payload.trackLabel')))
+          yield put(trackEventAction(get(action, 'payload.trackLabel')))
         }
         break
       case ACTION_TYPES.COMMENT.CREATE_REQUEST:
-        return yield put(trackEventAction('published_comment'))
+        yield put(trackEventAction('published_comment'))
+        break
       case ACTION_TYPES.COMMENT.DELETE_REQUEST:
-        return yield put(trackEventAction('deleted_comment'))
+        yield put(trackEventAction('deleted_comment'))
+        break
       case ACTION_TYPES.OMNIBAR.OPEN:
-        return yield put(trackEventAction('opened_omnibar'))
-      case ACTION_TYPES.POST.CREATE_REQUEST:
+        yield put(trackEventAction('opened_omnibar'))
+        break
+      case ACTION_TYPES.POST.CREATE_REQUEST: {
+        const isFeatured = yield select(selectProfileIsFeatured)
         if (get(action, 'payload.body.body[0].link_url', '').length) {
           yield put(trackEventAction('added_buy_button'))
         }
-        return yield put(trackEventAction(get(action, 'meta.repostId') ? 'published_repost' : 'published_post', { isFeatured }))
+        yield put(trackEventAction(get(action, 'meta.repostId') ? 'published_repost' : 'published_post', { isFeatured }))
+        break
+      }
       case ACTION_TYPES.POST.DELETE_REQUEST:
-        return yield put(trackEventAction('deleted_post'))
-      case ACTION_TYPES.POST.LOVE_REQUEST:
+        yield put(trackEventAction('deleted_post'))
+        break
+      case ACTION_TYPES.POST.LOVE_REQUEST: {
+        const method = get(action, 'payload.method')
         if (method === 'POST') {
-          return yield put(trackEventAction('web_production.post_actions_love'))
+          yield put(trackEventAction('web_production.post_actions_love'))
         }
         break
-      case ACTION_TYPES.POST.WATCH_REQUEST:
+      }
+      case ACTION_TYPES.POST.WATCH_REQUEST: {
+        const method = get(action, 'payload.method')
         if (method === 'DELETE') {
-          return yield put(trackEventAction('unwatched-post'))
+          yield put(trackEventAction('unwatched-post'))
+          break
         }
-        return yield put(trackEventAction('watched-post'))
+        yield put(trackEventAction('watched-post'))
+        break
+      }
       case ACTION_TYPES.POST.UPDATE_REQUEST:
-        return yield put(trackEventAction('edited_post'))
+        yield put(trackEventAction('edited_post'))
+        break
       case ACTION_TYPES.PROFILE.DELETE_REQUEST:
-        return yield put(trackEventAction('user-deleted-account'))
+        yield put(trackEventAction('user-deleted-account'))
+        break
       case ACTION_TYPES.PROFILE.FOLLOW_CATEGORIES_REQUEST:
-        return yield put(trackEventAction('Onboarding.Settings.Categories.Completed',
+        yield put(trackEventAction('Onboarding.Settings.Categories.Completed',
           { categories: get(action, 'payload.body.followed_category_ids', []).length },
         ))
+        break
       case ACTION_TYPES.PROFILE.SIGNUP_SUCCESS:
-        return yield put(trackEventAction('join-successful'))
+        yield put(trackEventAction('join-successful'))
+        break
       case ACTION_TYPES.RELATIONSHIPS.UPDATE_INTERNAL:
       case ACTION_TYPES.RELATIONSHIPS.UPDATE_REQUEST:
         switch (get(action, 'payload.priority')) {
           case RELATIONSHIP_PRIORITY.FRIEND:
           case RELATIONSHIP_PRIORITY.NOISE:
-            return yield put(trackEventAction('followed_user'))
+            yield put(trackEventAction('followed_user'))
+            break
           case RELATIONSHIP_PRIORITY.INACTIVE:
           case RELATIONSHIP_PRIORITY.NONE:
-            return yield put(trackEventAction('unfollowed_user'))
+            yield put(trackEventAction('unfollowed_user'))
+            break
           case RELATIONSHIP_PRIORITY.BLOCK:
-            return yield put(trackEventAction('blocked_user'))
+            yield put(trackEventAction('blocked_user'))
+            break
           case RELATIONSHIP_PRIORITY.MUTE:
-            return yield put(trackEventAction('muted_user'))
+            yield put(trackEventAction('muted_user'))
+            break
           default:
             break
         }
         break
       case ACTION_TYPES.USER.COLLAB_WITH_REQUEST:
-        return yield put(trackEventAction('send-collab-dialog-profile'))
+        yield put(trackEventAction('send-collab-dialog-profile'))
+        break
       case ACTION_TYPES.USER.HIRE_ME_REQUEST:
-        return yield put(trackEventAction('send-hire-dialog-profile'))
+        yield put(trackEventAction('send-hire-dialog-profile'))
+        break
       default:
         break
     }
