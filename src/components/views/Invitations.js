d2a23ce8b7a2317c6ae83b328dc821a8da563278
Ryan Boyajian <ryan.e.boyajian@gmail.com>
2017-06-27 16:06:19 -0600

    Update the styling of invitations
    [Finishes #147303063](https://www.pivotaltracker.com/story/show/147303063)

--- src/components/views/Invitations.js
+++ src/components/views/Invitations.js
@@ -3,31 +3,79 @@ import PropTypes from 'prop-types'
 import { MainView } from '../views/MainView'
 import StreamContainer from '../../containers/StreamContainer'
 import InvitationFormContainer from '../../containers/InvitationFormContainer'
-import { css } from '../../styles/jss'
 import * as s from '../../styles/jso'
+import { css, media } from '../../styles/jss'
 
 const mainStyle = css(
   s.mxAuto,
   { maxWidth: 1440 },
+  media(
+    s.minBreak2,
+    s.flex1,
+    s.flexRow,
+    s.justifySpaceAround,
+    s.itemsStart,
+  ),
 )
 
 const columnStyle = css(
-  { width: '50%' },
-  s.inlineBlock,
+  { margin: '20px 10px 10px 10px' },
+  media(
+    s.minBreak2,
+    s.fullWidth,
+    { margin: '40px 20px 20px' },
+  ),
+  media(
+    s.minBreak4,
+    s.mx40,
+  ),
+)
+
+const headerStyle = css(
+  s.mb40,
+)
+
+const headingStyle = css(
+  s.sansBlack,
+  s.fontSize18,
+  s.lh30,
+  s.hv30,
+  s.nowrap,
+  media(
+    s.minBreak2,
+    s.hv40,
+    s.fontSize24,
+    s.lh40,
+  ),
+)
+
+const streamHeadingStyle = css(
+  { ...headingStyle },
+  s.my40,
+  media(
+    s.minBreak2,
+    s.mt0,
+    { marginBottom: 75 },
+  ),
+)
+
+const invitedStreamStyle = css(
+  { padding: '0 !important' },
+  s.mt40,
 )
 
 export const Invitations = ({ streamAction }) =>
   <MainView className={`${mainStyle}`}>
     <div className={columnStyle}>
-      <header className="InvitationsHeader">
-        <h1 className="InvitationsHeading">Invite some cool people</h1>
+      <header className={headerStyle}>
+        <h1 className={headingStyle}>Invite some cool people</h1>
         <p>Help Ello grow.</p>
       </header>
       <InvitationFormContainer />
     </div>
     <div className={columnStyle}>
-      <h2 className="InvitationsStreamHeading">{'People you\'ve invited'}</h2>
-      <StreamContainer action={streamAction} />
+      <h2 className={streamHeadingStyle}>{'People you\'ve invited'}</h2>
+      <StreamContainer className={`${invitedStreamStyle}`} action={streamAction} />
     </div>
   </MainView>
 
