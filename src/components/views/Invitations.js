1b80c8e895d8176622fc8068492396e6a2080744
Ryan Boyajian <ryan.e.boyajian@gmail.com>
2017-06-27 09:34:06 -0600

    WIP: restyling invitations into a 2 column layout
    [#147303063](https://www.pivotaltracker.com/story/show/147303063)

--- src/components/views/Invitations.js
+++ src/components/views/Invitations.js
@@ -3,16 +3,32 @@ import PropTypes from 'prop-types'
 import { MainView } from '../views/MainView'
 import StreamContainer from '../../containers/StreamContainer'
 import InvitationFormContainer from '../../containers/InvitationFormContainer'
+import { css } from '../../styles/jss'
+import * as s from '../../styles/jso'
+
+const mainStyle = css(
+  s.mxAuto,
+  { maxWidth: 1440 },
+)
+
+const columnStyle = css(
+  { width: '50%' },
+  s.inlineBlock,
+)
 
 export const Invitations = ({ streamAction }) =>
-  <MainView className="Invitations">
-    <header className="InvitationsHeader">
-      <h1 className="InvitationsHeading">Invite some cool people</h1>
-      <p>Help Ello grow.</p>
-    </header>
-    <InvitationFormContainer />
-    <h2 className="InvitationsStreamHeading">{'People you\'ve invited'}</h2>
-    <StreamContainer action={streamAction} />
+  <MainView className={`${mainStyle}`}>
+    <div className={columnStyle}>
+      <header className="InvitationsHeader">
+        <h1 className="InvitationsHeading">Invite some cool people</h1>
+        <p>Help Ello grow.</p>
+      </header>
+      <InvitationFormContainer />
+    </div>
+    <div className={columnStyle}>
+      <h2 className="InvitationsStreamHeading">{'People you\'ve invited'}</h2>
+      <StreamContainer action={streamAction} />
+    </div>
   </MainView>
 
 Invitations.propTypes = {
