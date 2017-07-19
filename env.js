d779cad06db1484d3d3524a7bb7191d9e1e84ce8
Ryan Boyajian <ryan.e.boyajian@gmail.com>
2017-07-19 10:52:22 -0600

    Pull env from meta tag with fallback to window

--- env.js
+++ env.js
@@ -1,4 +1,18 @@
-if (typeof window !== 'undefined' && typeof window.webappEnv !== 'undefined') {
+const envEl = document.querySelector('meta[name="webappEnv"]')
+if (envEl) {
+  const webappEnv = JSON.parse(decodeURI(envEl.getAttribute('content')))
+  module.exports = {
+    API_DOMAIN: webappEnv.AUTH_DOMAIN,
+    APP_DEBUG: webappEnv.APP_DEBUG,
+    AUTH_CLIENT_ID: webappEnv.AUTH_CLIENT_ID,
+    AUTH_DOMAIN: webappEnv.AUTH_DOMAIN,
+    LOGO_MARK: webappEnv.LOGO_MARK,
+    NODE_ENV: process.env.NODE_ENV,
+    PROMO_HOST: webappEnv.PROMO_HOST,
+    SEGMENT_WRITE_KEY: webappEnv.SEGMENT_WRITE_KEY,
+    USE_LOCAL_EMOJI: (process.env.USE_LOCAL_EMOJI === 'true'),
+  }
+} else if (typeof window !== 'undefined' && typeof window.webappEnv !== 'undefined') {
   module.exports = {
     API_DOMAIN: window.webappEnv.AUTH_DOMAIN,
     APP_DEBUG: window.webappEnv.APP_DEBUG,
