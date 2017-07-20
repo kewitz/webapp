332dfa61fd21ab93053e0ddf0d455afae6c1af33
Ryan Boyajian <ryan.e.boyajian@gmail.com>
2017-07-20 11:31:57 -0600

    Fix some env vars for dev proxying

--- env.js
+++ env.js
@@ -2,7 +2,7 @@ const envEl = document.querySelector('meta[name="webappEnv"]')
 if (envEl) {
   const webappEnv = JSON.parse(decodeURI(envEl.getAttribute('content')))
   module.exports = {
-    API_DOMAIN: webappEnv.AUTH_DOMAIN,
+    API_DOMAIN: webappEnv.API_DOMAIN || webappEnv.AUTH_DOMAIN,
     APP_DEBUG: webappEnv.APP_DEBUG,
     AUTH_CLIENT_ID: webappEnv.AUTH_CLIENT_ID,
     AUTH_DOMAIN: webappEnv.AUTH_DOMAIN,
@@ -14,7 +14,7 @@ if (envEl) {
   }
 } else if (typeof window !== 'undefined' && typeof window.webappEnv !== 'undefined') {
   module.exports = {
-    API_DOMAIN: window.webappEnv.AUTH_DOMAIN,
+    API_DOMAIN: window.webappEnv.API_DOMAIN || window.webappEnv.AUTH_DOMAIN,
     APP_DEBUG: window.webappEnv.APP_DEBUG,
     AUTH_CLIENT_ID: window.webappEnv.AUTH_CLIENT_ID,
     AUTH_DOMAIN: window.webappEnv.AUTH_DOMAIN,
@@ -26,7 +26,7 @@ if (envEl) {
   }
 } else {
   module.exports = {
-    API_DOMAIN: process.env.API_DOMAIN,
+    API_DOMAIN: process.env.API_DOMAIN || process.env.AUTH_DOMAIN,
     APP_DEBUG: (process.env.APP_DEBUG === 'true'),
     AUTH_CLIENT_ID: process.env.AUTH_CLIENT_ID,
     AUTH_CLIENT_SECRET: process.env.AUTH_CLIENT_SECRET,
